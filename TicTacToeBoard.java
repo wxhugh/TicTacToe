8f39813a8bf3371741f418d517da835dcf8aaf68
wxhugh <wxhugh411@wxhugh411-VirtualBox.(none)>
2016-03-08 11:19:28 -0500

    started game player

--- TicTacToeBoard.java
+++ TicTacToeBoard.java
@@ -1,8 +1,6 @@
 import java.util.*;
 
 class TicTacToeBoard {
-	Scanner player1 = new Scanner(System.in);
-	Scanner player2 = new Scanner(System.in);
 	private String[][] board;
 	public TicTacToeBoard() {
 		board = new String[3][3];
@@ -12,22 +10,7 @@ class TicTacToeBoard {
 			}
 		}
 	}
-	public String gameSetup() {
-		System.out.println("Hello Player One. Do you want to be X or O?(Use X and O)");
-		String answer = player1.next();
-		String other = "";
-		if (answer.equals("X")) {
-			other = "O";
-		}
-		else {
-			other = "X";
-		}
-		System.out.println("Player 1 is " + answer + " and Player 2 is " + other + ".");
-		return answer;
-	}
-	public String gamePlay() {
-
-	}
+	
 	public String toString(){
 		return (board[0][0] + "|" + board[0][1] + "|" + board[0][2] + "\n"
 		 +  board[1][0] + "|" + board[1][1] + "|" + board[1][2] + "\n" 
--- TicTacToeTester.java
+++ TicTacToeTester.java
@@ -1,7 +1,40 @@
+import java.util.*;
+
 class TicTacToeTester {
+	Scanner p1 = new Scanner(System.in);
+	Scanner p2 = new Scanner(System.in);
+	Scanner answers = new Scanner(System.in);
+	int answer = 0;
+	String player1 = "";
+	String player2 = "";
+	public static String gameSetup() {
+		System.out.println("Hello Player One. Do you want to be X or O?(Use X and O)");
+		player1 = p1.next();
+		if (player1.equals("X")) {
+			player2 = "O";
+		}
+		else {
+			player2 = "X";
+		}
+		System.out.println("Player 1 is " + player1 + " and Player 2 is " + player2 + ".");
+		return player1;
+	}
+	public static String gamePlay() {
+		System.out.println("What row do want your X/O placed in?(Top, Mid, Bot)");
+		answer = answers.next();
+		if (answer.equals("Top")) {
+			System.out.println("What row do want your X/O placed in?(Left, Mid, Right)");
+			answer = answers.next();
+			
+		}
+
+
+
+		
+	}
 	public static void main(String[] args) {
 		TicTacToeBoard board1 = new TicTacToeBoard();
-		board1.gameSetup();
+		gameSetup();
 		System.out.println(board1.toString());
 	}
 }
