2eecd51adb58016be10b2ead420bbedaf1f65488
wxhugh <wxhugh411@wxhugh411-VirtualBox.(none)>
2016-03-10 11:20:21 -0500

    fix setup of players

--- TicTacToeTester.java
+++ TicTacToeTester.java
@@ -6,6 +6,8 @@ class TicTacToeTester {
 	static String answer = "";
 	static String player1 = "";
 	static String player2 = "";
+	static boolean win;
+	static String winner = "";
 	public static String gameSetup() {
 		System.out.println("Hello Player One. Do you want to be X or O?(X and O)");
 		player1 = answers.next();
@@ -18,19 +20,21 @@ class TicTacToeTester {
 		System.out.println("Player 1 is " + player1 + " and Player 2 is " + player2 + ".");
 		return player1;
 	}
+	
 	public static String gamePlay() {
 		int row = 0;
 		int col = 0;
-		System.out.println("What row do want your X/O placed in?(top, mid, bot)");
+		while (win == false) {
+			System.out.println("What row do want your X/O placed in?(top, mid, bot)");
 		answer = answers.next();
 		if (answer.equals("top")) {
 			row = 0;
-			System.out.println("What row do want your X/O placed in?(left, mid, right)");
+			System.out.println("What row do want your X/O placed in?(left, center, right)");
 			answer = answers.next();
 			if (answer.equals("left")) {
 				col = 0;
 			}
-			if (answer.equals("mid")) {
+			if (answer.equals("center")) {
 				col = 1;
 			}
 			if (answer.equals("right")) {
@@ -39,13 +43,13 @@ class TicTacToeTester {
 			board1.board[row][col] = player1;		
 		}
 		if (answer.equals("mid")) {
-			row = 0;
-			System.out.println("What row do want your X/O placed in?(left, mid, right)");
+			row = 1;
+			System.out.println("What row do want your X/O placed in?(left, center, right)");
 			answer = answers.next();
 			if (answer.equals("left")) {
 				col = 0;
 			}
-			if (answer.equals("mid")) {
+			if (answer.equals("center")) {
 				col = 1;
 			}
 			if (answer.equals("right")) {
@@ -54,13 +58,13 @@ class TicTacToeTester {
 			board1.board[row][col] = player1;		
 		}
 		if (answer.equals("bot")) {
-			row = 0;
-			System.out.println("What row do want your X/O placed in?(left, mid, right)");
+			row = 2;
+			System.out.println("What row do want your X/O placed in?(left, center, right)");
 			answer = answers.next();
 			if (answer.equals("left")) {
 				col = 0;
 			}
-			if (answer.equals("mid")) {
+			if (answer.equals("center")) {
 				col = 1;
 			}
 			if (answer.equals("right")) {
@@ -68,15 +72,56 @@ class TicTacToeTester {
 			}
 			board1.board[row][col] = player1;		
 		}
-		System.out.println(board);
+		System.out.println(board1.toString());
+		checkWin();
+		}
 		return answer;
-		
+	}
+	public static boolean checkWin() {
+		int letterCount = 0;
+		if (win == false) {
+			for (int i = 0;i < board1.board.length;i++) {
+				for (int j = 0;j < board1.board.length;j++) {
+					if (board1.board[i][j] == player1) {
+						letterCount++;
+						if (letterCount == 3) {
+							win = true;
+							winner = player1;
+						}
+					}	
+					if (board1.board[i][j] == player2) {
+						letterCount++;
+						if (letterCount == 3) {
+							win = true;
+							winner = player2;
+						}
+					}
+					/*if (board1.board[j][i] == player1) {
+						letterCount++;	
+						if (letterCount == 3) {
+							win = true;
+							winner = player1;
+						}
+					}	
+					if (board1.board[j][i] == player2) {
+						letterCount++;
+						if (letterCount == 3) {
+							win = true;
+							winner = player2;
+						}
+					}*/
+				}
+			}
+		}
+	return win;	
 	}
 
-	}
+
+	
 	public static void main(String[] args) {
 		gameSetup();
 		System.out.println(board1.toString());
 		gamePlay();
 	}
+	
 }
